c1cd7ff22fd8c085667bfb4e7f6d01a05df379a3
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-13 10:37:43 -0700

    Testing code for deployment to heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // /* Handlebars template */
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const express = require('express');
 // const routes = require('./controller/');
-// const sequelize = require('./config/connection');
+const sequelize = require('./config/connection');
 // /* Linking the stylesheet */
-// const path = require('path');
+const path = require('path');
 
 // Initialize the app and create a port
 const app = express();
@@ -12,42 +12,43 @@ const PORT = process.env.PORT || 3001;
 // for express-sessions library - store user cookies
 const session = require('express-session');
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-// Linking the style sheet //
-// app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+//Linking the style sheet //
+app.use(express.static(path.join(__dirname, 'public')));
 
 // //Front end code ( )
-// app.use(express.static('public'));
+app.use(express.static('public'));
 // to turn on routes for DB -back end
 // app.use(routes);
 
 //setup routes
 app.get('/', (req, res) => {
+  res.render("login")
     // res.sendFile(path.join(__dirname, './public/index.html'));
-    res.send("server started!")
+    // res.send("server started!")
   });
   
 // Start the server on the port
