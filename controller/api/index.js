cef8a89fcb5674c6edc870a6d13e87d27dd1f4c1
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-13 11:12:21 -0700

    user refs commented out

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,12 +1,12 @@
 // serves to collect all the API routes and package them up
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+// const userRoutes = require('./user-routes');
 // const reviewRoutes = require('./review-routes');
 const commentRoutes = require('./comment-routes');
 const postRoutes = require('./post-routes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 // router.use('/reviews', reviewRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
