c668a68653592ef0c34416061d8abde20f8838a1
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-12 23:34:32 -0700

    review cancelled

--- controller/api/review-routes.js
+++ controller/api/review-routes.js
@@ -1,130 +1,130 @@
-const sequelize = require('../../config/connection');
-const router = require('express').Router();
-const { Review, User, Vote, Post, Comment } = require('../../models');
+// const sequelize = require('../../config/connection');
+// const router = require('express').Router();
+// const { Review, User, Vote, Post, Comment } = require('../../models');
 
-// get all users
-router.get('/', (req, res) => {
-  console.log('======================');
-  Review.findAll({
-    order: [['created_at', 'DESC']],
-    attributes: [
-      'id',
-      'review_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE review.id = vote.review_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-   })
-    .then(dbReviewData => res.json(dbReviewData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// // get all users
+// router.get('/', (req, res) => {
+//   console.log('======================');
+//   Review.findAll({
+//     order: [['created_at', 'DESC']],
+//     attributes: [
+//       'id',
+//       'review_url',
+//       'title',
+//       'created_at',
+//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE review.id = vote.review_id)'), 'vote_count']
+//     ],
+//     include: [
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//    })
+//     .then(dbReviewData => res.json(dbReviewData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-router.get('/:id', (req, res) => {
-  Review.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'review_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE review.id = vote.review_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-   })
-    .then(dbReviewData => {
-      if (!dbReviewData) {
-        res.status(404).json({ message: 'No review found with this id' });
-        return;
-      }
-      res.json(dbReviewData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.get('/:id', (req, res) => {
+//   Review.findOne({
+//     where: {
+//       id: req.params.id
+//     },
+//     attributes: [
+//       'id',
+//       'review_url',
+//       'title',
+//       'created_at',
+//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE review.id = vote.review_id)'), 'vote_count']
+//     ],
+//     include: [
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//    })
+//     .then(dbReviewData => {
+//       if (!dbReviewData) {
+//         res.status(404).json({ message: 'No review found with this id' });
+//         return;
+//       }
+//       res.json(dbReviewData);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-router.post('/', (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-  Review.create({
-    title: req.body.title,
-    review_url: req.body.review_url,
-    user_id: req.body.user_id
-  })
-    .then(dbReviewData => res.json(dbReviewData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.post('/', (req, res) => {
+//   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+//   Review.create({
+//     title: req.body.title,
+//     review_url: req.body.review_url,
+//     user_id: req.body.user_id
+//   })
+//     .then(dbReviewData => res.json(dbReviewData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Review.upvote(req.body, { Vote })
-    .then(updatedReviewData => res.json(updatedReviewData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
+// router.put('/upvote', (req, res) => {
+//   // custom static method created in models/Post.js
+//   Review.upvote(req.body, { Vote })
+//     .then(updatedReviewData => res.json(updatedReviewData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(400).json(err);
+//     });
+// });
 
-router.put('/:id', (req, res) => {
-  Review.update(
-    {
-      title: req.body.title
-    },
-    {
-      where: {
-        id: req.params.id
-      }
-    }
-  )
-    .then(dbReviewData => {
-      if (!dbReviewData) {
-        res.status(404).json({ message: 'No review found with this id' });
-        return;
-      }
-      res.json(dbReviewData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.put('/:id', (req, res) => {
+//   Review.update(
+//     {
+//       title: req.body.title
+//     },
+//     {
+//       where: {
+//         id: req.params.id
+//       }
+//     }
+//   )
+//     .then(dbReviewData => {
+//       if (!dbReviewData) {
+//         res.status(404).json({ message: 'No review found with this id' });
+//         return;
+//       }
+//       res.json(dbReviewData);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-router.delete('/:id', (req, res) => {
-  Review.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbReviewData => {
-      if (!dbReviewData) {
-        res.status(404).json({ message: 'No review found with this id' });
-        return;
-      }
-      res.json(dbReviewData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.delete('/:id', (req, res) => {
+//   Review.destroy({
+//     where: {
+//       id: req.params.id
+//     }
+//   })
+//     .then(dbReviewData => {
+//       if (!dbReviewData) {
+//         res.status(404).json({ message: 'No review found with this id' });
+//         return;
+//       }
+//       res.json(dbReviewData);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-module.exports = router;
+// module.exports = router;
