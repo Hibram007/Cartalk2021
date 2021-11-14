11a55727eba0bee738c52fbc45e9654262275e79
Nicholas Dixon <whitelotus32@Nicholass-MBP.attlocal.net>
2021-11-14 13:39:03 -0800

    Car Data API

--- public/javascript/car-data.js
+++ public/javascript/car-data.js
@@ -1,16 +1,13 @@
-const data = null;
-
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+fetch("https://car-data.p.rapidapi.com/cars/types", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "car-data.p.rapidapi.com",
+		"x-rapidapi-key": "f11be51242msh61b23021d1395ebp1de438jsn33ad5f25234c"
 	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
 });
-
-xhr.open("GET", "https://car-data.p.rapidapi.com/cars/types");
-xhr.setRequestHeader("x-rapidapi-host", "car-data.p.rapidapi.com");
-xhr.setRequestHeader("x-rapidapi-key", "f11be51242msh61b23021d1395ebp1de438jsn33ad5f25234c");
-
-xhr.send(data);
